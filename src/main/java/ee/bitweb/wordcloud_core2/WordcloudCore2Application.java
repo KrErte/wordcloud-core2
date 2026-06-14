7e68ec7cf8313b56f41dca1956d6efa2ed28b11d
Kristo Erte <kristo.erte@gmail.com>
2026-06-14 11:49:14 +0300

    Init new Spring Boot project

---
+++ src/main/java/ee/bitweb/wordcloud_core2/WordcloudCore2Application.java
@@ -0,0 +1,13 @@
+package ee.bitweb.wordcloud_core2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcloudCore2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcloudCore2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ee/bitweb/wordcloud_core2/WordcloudCore2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bitweb.wordcloud_core2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordcloudCore2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
