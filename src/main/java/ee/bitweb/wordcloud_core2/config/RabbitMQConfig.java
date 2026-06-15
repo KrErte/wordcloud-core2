f66872e209501979d7a8387b4e691624c50f5d47
Kristo Erte <kristo.erte@gmail.com>
2026-06-15 12:36:31 +0300

    Created config files

---
+++ src/main/java/ee/bitweb/wordcloud_core2/config/RabbitMQConfig.java
@@ -0,0 +1,49 @@
+package ee.bitweb.wordcloud_core2.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${wordcloud.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${wordcloud.rabbitmq.queue}")
+    private String queue;
+
+    @Value("${wordcloud.rabbitmq.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public DirectExchange wordcloudExchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Queue wordcloudQueue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public Binding wordcloudBinding() {
+        return BindingBuilder.bind(wordcloudQueue()).to(wordcloudExchange()).with(routingKey);
+    }
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
